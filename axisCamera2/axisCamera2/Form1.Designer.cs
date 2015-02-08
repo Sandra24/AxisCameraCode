8831c21e7732cc67a8953472809d292ebae2e198
Sandra24 <sandrawiedmann24@gmail.com>
2015-02-08 12:57:06 -0500

    camera code

---
+++ axisCamera2/axisCamera2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace axisCamera2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ipAdress = new System.Windows.Forms.TextBox();
+            this.btnLiveView = new System.Windows.Forms.Button();
+            this.amc = new AxAXISMEDIACONTROLLib.AxAxisMediaControl();
+            this.amc2 = new AxAXISMEDIACONTROLLib.AxAxisMediaControl();
+            this.ipAdress2 = new System.Windows.Forms.TextBox();
+            this.btnLiveView2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.amc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amc2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ipAdress
+            // 
+            this.ipAdress.Location = new System.Drawing.Point(12, 8);
+            this.ipAdress.Name = "ipAdress";
+            this.ipAdress.Size = new System.Drawing.Size(111, 20);
+            this.ipAdress.TabIndex = 0;
+            // 
+            // btnLiveView
+            // 
+            this.btnLiveView.Location = new System.Drawing.Point(198, 3);
+            this.btnLiveView.Name = "btnLiveView";
+            this.btnLiveView.Size = new System.Drawing.Size(97, 25);
+            this.btnLiveView.TabIndex = 1;
+            this.btnLiveView.Text = "Live View";
+            this.btnLiveView.UseVisualStyleBackColor = true;
+            this.btnLiveView.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // amc
+            // 
+            this.amc.Enabled = true;
+            this.amc.Location = new System.Drawing.Point(12, 46);
+            this.amc.Name = "amc";
+            this.amc.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("amc.OcxState")));
+            this.amc.Size = new System.Drawing.Size(415, 409);
+            this.amc.TabIndex = 2;
+            // 
+            // amc2
+            // 
+            this.amc2.Enabled = true;
+            this.amc2.Location = new System.Drawing.Point(458, 47);
+            this.amc2.Name = "amc2";
+            this.amc2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("amc2.OcxState")));
+            this.amc2.Size = new System.Drawing.Size(409, 408);
+            this.amc2.TabIndex = 3;
+            // 
+            // ipAdress2
+            // 
+            this.ipAdress2.Location = new System.Drawing.Point(458, 8);
+            this.ipAdress2.Name = "ipAdress2";
+            this.ipAdress2.Size = new System.Drawing.Size(109, 20);
+            this.ipAdress2.TabIndex = 4;
+            // 
+            // btnLiveView2
+            // 
+            this.btnLiveView2.Location = new System.Drawing.Point(636, 8);
+            this.btnLiveView2.Name = "btnLiveView2";
+            this.btnLiveView2.Size = new System.Drawing.Size(99, 29);
+            this.btnLiveView2.TabIndex = 5;
+            this.btnLiveView2.Text = "Live View";
+            this.btnLiveView2.UseVisualStyleBackColor = true;
+            this.btnLiveView2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 475);
+            this.Controls.Add(this.btnLiveView2);
+            this.Controls.Add(this.ipAdress2);
+            this.Controls.Add(this.amc2);
+            this.Controls.Add(this.amc);
+            this.Controls.Add(this.btnLiveView);
+            this.Controls.Add(this.ipAdress);
+            this.Name = "Form1";
+            this.Text = "Camera View";
+            ((System.ComponentModel.ISupportInitialize)(this.amc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.amc2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipAdress;
+        private System.Windows.Forms.Button btnLiveView;
+        private AxAXISMEDIACONTROLLib.AxAxisMediaControl amc;
+        private AxAXISMEDIACONTROLLib.AxAxisMediaControl amc2;
+        private System.Windows.Forms.TextBox ipAdress2;
+        private System.Windows.Forms.Button btnLiveView2;
+    }
+}
+
---
+++ axisCamera2/axisCamera2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AXISMEDIACONTROLLib;
+
+namespace axisCamera2
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+            amc.StretchToFit = true;
+            amc2.StretchToFit = true;
+            amc.VideoRenderer = (int)AMC_VIDEO_RENDERER.AMC_VIDEO_RENDERER_VMR9;
+            amc2.VideoRenderer = (int)AMC_VIDEO_RENDERER.AMC_VIDEO_RENDERER_VMR9;
+        }
+        private string CompleteURL(string theMediaURL, string theMediaType)
+        {
+            string anURL = theMediaURL;
+            anURL += "/axis-cgi/mjpg/video.cgi";     
+            anURL = CompleteProtocol(anURL, theMediaType);
+            return anURL;
+        }
+        private string CompleteProtocol(string theMediaURL, string theMediaType)
+        {
+            if (theMediaURL.IndexOf("://") >= 0) return theMediaURL;
+
+            string anURL = theMediaURL;
+            anURL = "http://" + anURL;
+            return anURL;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string media = "mjpeg";
+            amc.Stop();
+            amc.MediaUsername = "FRC";
+            amc.MediaPassword = "FRC";
+            amc.MediaURL = CompleteURL(ipAdress.Text, media);
+            amc.Play();         
+        }
+        private void button2_Click(object sender, EventArgs e) 
+        {
+            string media = "mjpeg";
+            amc2.Stop();
+            amc2.MediaUsername = "FRC";
+            amc2.MediaPassword = "FRC";
+            amc2.MediaURL = CompleteURL(ipAdress2.Text, media);
+            amc2.Play();
+        }
+        
+    }
+}
